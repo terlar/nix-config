77dbdd6f429812af43fa521a33940ad984c4722f
Terje Larsen <terlar@gmail.com>
2018-02-05 23:25:19 +0100

    gui: use gnome3 as desktop manager

--- gui.nix
+++ gui.nix
@@ -21,67 +21,68 @@
       hyper
       dropbox-cli
       kde-gtk-config
-      arc-theme
-      arc-icon-theme
+      numix-gtk-theme
       paper-gtk-theme
       paper-icon-theme
     ];
 
     shellInit = ''
-      export GTK_PATH=$GTK_PATH:${pkgs.paper-gtk-theme}/lib/gtk-2.0
-      export GTK2_RC_FILES=$GTK2_RC_FILES:${pkgs.paper-gtk-theme}/share/themes/Paper/gtk-2.0/gtkrc
+      export GTK_PATH=$GTK_PATH:${pkgs.numix-gtk-theme}/lib/gtk-2.0
+      export GTK2_RC_FILES=$GTK2_RC_FILES:${pkgs.numix-gtk-theme}/share/themes/Numix/gtk-2.0/gtkrc
     '';
   };
 
   # Enable the X11 windowing system.
-  services.xserver =
-    { enable = true;
-      autorun = true;
+  services.xserver = {
+    enable = true;
+    autorun = true;
 
-      videoDrivers = [ "intel" ];
+    videoDrivers = [ "intel" ];
 
-      # Keyboard
-      layout = "us";
-      xkbVariant = "altgr-intl";
-      xkbOptions = "lv3:ralt_switch,ctrl:nocaps";
+    # Keyboard
+    layout = "us";
+    xkbVariant = "altgr-intl";
+    xkbOptions = "lv3:ralt_switch,ctrl:nocaps";
 
-      # Enable touchpad support.
-      libinput =
-        { enable = true;
-        naturalScrolling = true;
-        disableWhileTyping = true;
-      };
-
-      # Enable desktop environment.
-      displayManager.lightdm.enable = true;
-      windowManager.i3 =
-        { enable = true;
-          package = pkgs.i3-gaps;
-        };
-
-      monitorSection =
-        ''
-        DisplaySize 508 284
-        '';
+    # Enable touchpad support.
+    libinput = {
+      enable = true;
+      naturalScrolling = true;
+      disableWhileTyping = true;
     };
+
+    # Enable desktop environment.
+    desktopManager = {
+      gnome3.enable = true;
+      default = "gnome3";
+    };
+    windowManager.i3 = {
+      enable = true;
+      package = pkgs.i3-gaps;
+    };
+
+    monitorSection = ''
+      DisplaySize 508 284
+    '';
+  };
 
   # Setup fonts
-  fonts =
-    { enableFontDir = true;
-      fonts = with pkgs;
-        [ unifont
-          symbola
-          noto-fonts
-          noto-fonts-cjk
-          noto-fonts-emoji
-          fira
-          fira-mono
-          fira-code
-          fantasque-sans-mono
-          emojione
-          emacs-all-the-icons-fonts
-          source-han-sans-simplified-chinese
-          source-han-sans-traditional-chinese
-        ];
-    };
+  fonts = {
+    enableFontDir = true;
+    fonts = with pkgs; [
+      unifont
+      symbola
+      noto-fonts
+      noto-fonts-cjk
+      noto-fonts-emoji
+      fira
+      fira-mono
+      fira-code
+      fantasque-sans-mono
+      emojione
+      emacs-all-the-icons-fonts
+      source-han-sans-simplified-chinese
+      source-han-sans-traditional-chinese
+    ];
+  };
 }
