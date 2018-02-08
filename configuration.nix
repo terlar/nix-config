67963e68ad98ede8e639144f761cab5a238bfba9
Terje Larsen <terlar@gmail.com>
2018-02-08 23:45:04 +0100

    config: update to emacs 27.0.50

--- configuration.nix
+++ configuration.nix
@@ -23,6 +23,26 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  nixpkgs.config.packageOverrides = super: {
+    emacs = pkgs.stdenv.lib.overrideDerivation (super.emacs.override { srcRepo = true; }) (oldAttrs: rec {
+      version = "27.0.50";
+      versionModifier = "-git";
+      name = "emacs-${version}${versionModifier}";
+
+	    withGtk2 = false;
+      withGtk3 = true;
+      withXwidgets = true;
+      imagemagick = pkgs.imagemagickBig;
+
+      src = pkgs.fetchgit {
+        url = "git://git.sv.gnu.org/emacs.git";
+        rev = "1cdd0e8cd801aa1d6f04ab4d8e6097a46af8c951";
+        sha256 = "0xgf63l4rlrabaxy6c2qxi76p3mg460c49b23g1rswigw2sc7c9f";
+      };
+      patches = [];
+    });
+  };
+
   system.autoUpgrade.enable = true;
   system.stateVersion = "17.09";
 }
