10b6ee133a5c0a3a45c958d8ad09c1ea0ebcdfd3
Terje Larsen <terlar@gmail.com>
2018-12-01 10:00:40 +0100

    dev(python): replace autopep8 with black

--- dev/python.nix
+++ dev/python.nix
@@ -5,8 +5,8 @@
     python
     python27Packages.pip
     python36Packages.pip
-    python27Packages.autopep8
-    python36Packages.autopep8
+    python27Packages.black
+    python36Packages.black
     python27Packages.flake8
     python36Packages.flake8
     python27Packages.pytest
