49f3f484a2b43d2d964f2cf654370be5cf4db60d
Terje Larsen <terlar@gmail.com>
2018-12-10 15:32:02 +0100

    nixos: update font config

--- config/nixos.nix
+++ config/nixos.nix
@@ -7,8 +7,19 @@
   i18n.consoleKeyMap = "us";
 
   fonts = {
-    fontconfig.enable = true;
-    fontconfig.dpi = 180;
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+
     fonts = import ./fonts.nix { inherit pkgs; };
+
+    fontconfig = {
+      enable = true;
+      dpi = 180;
+      defaultFonts = {
+        monospace = [ "Iosevka Slab" ];
+        sansSerif = [ "Noto Sans" ];
+        serif     = [ "Noto Serif" ];
+      };
+    };
   };
 }
