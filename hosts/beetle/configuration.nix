75d474d4edee7b01babf4b27a3b6a61753dd10a5
Terje Larsen <terlar@gmail.com>
2019-06-09 23:56:36 +0200

    sync: update dependencies
    Lock Linux Kernel at 5.1, as it seems compiling 5.2 is broken at the
    moment for me. Hangs the computer every time.

--- hosts/beetle/configuration.nix
+++ hosts/beetle/configuration.nix
@@ -24,7 +24,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use the newer but stable kernel packages.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_5_1;
 
   # Kernel modules:
   boot.kernelModules = [
