c7a288deaac90ce2177ae10403f3b9c8c606c438
Terje Larsen <terlar@gmail.com>
2019-07-16 20:34:30 +0200

    home(luakit): use letters for hints

--- config/home.nix
+++ config/home.nix
@@ -243,8 +243,15 @@ in rec {
 
     configFile."luakit/userconf.lua".text = ''
       local vertical_tabs = require "vertical_tabs"
+
       local settings = require "settings"
       settings.vertical_tabs.sidebar_width = 120
+
+      local select = require "select"
+      select.label_maker = function ()
+        local chars = interleave("asdfg", "hjkl")
+        return trim(sort(reverse(chars)))
+      end
     '';
   };
 
