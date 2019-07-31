49ad67bbdcdb791b2979f6df38e8ff43f73d0345
Terje Larsen <terlar@gmail.com>
2019-07-31 14:44:00 +0200

    home: use emacs for PDFs by default

--- config/home.nix
+++ config/home.nix
@@ -241,6 +241,7 @@ in rec {
       x-scheme-handler/http=qutebrowser.desktop
       x-scheme-handler/https=qutebrowser.desktop
       x-scheme-handler/ftp=qutebrowser.desktop
+      application/pdf=emacsclient.desktop
     '';
 
     configFile."luakit/userconf.lua".text = ''
@@ -254,6 +255,21 @@ in rec {
         local chars = interleave("asdfg", "hjkl")
         return trim(sort(reverse(chars)))
       end
+    '';
+
+    dataFile."applications/emacsclient.desktop".text = ''
+      [Desktop Entry]
+      Name=Emacsclient
+      GenericName=Text Editor
+      Comment=Edit text
+      MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;application/pdf;
+      Exec=${pkgs.scripts.emacseditor}/bin/emacseditor %F
+      Icon=emacs
+      Type=Application
+      Terminal=false
+      Categories=Development;TextEditor;
+      StartupWMClass=Emacs
+      Keywords=Text;Editor;
     '';
   };
 
