edc459ba4d2211cfa95d59bf9986eba856a20117
Terje Larsen <terlar@gmail.com>
2019-12-04 00:09:53 +0100

    overlays/menu: update

--- overlays/menu.nix
+++ overlays/menu.nix
@@ -4,9 +4,9 @@ let
   drv = pkgs.fetchFromGitHub {
     owner = "terlar";
     repo = "menu";
-    rev = "cb2a77f10f55f90021fbfb940cc000df2a63f989";
-    # date = 2019-11-30T19:15:17+01:00;
-    sha256 = "1h77w3cv2f12ih9sm8plzi53hyc05zx4spacbd8mc3gaa8k8md67";
+    rev = "a35743c7b6bcac7f829dc15678ee7e2385cb2330";
+    # date = 2019-12-04T00:39:29+01:00;
+    sha256 = "1s66wdnv8zbncyixc8dpz0zxj0xazkgdq3zj5hlg8xhgp0w56476";
   };
 in {
   menu = pkgs.callPackage drv { };
