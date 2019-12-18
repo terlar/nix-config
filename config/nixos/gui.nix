22ac51899e9a8f2df3ae66b2ab9d462aef04a10a
Terje Larsen <terlar@gmail.com>
2019-12-18 10:36:52 +0100

    config (i3): extract config in preparation to be shared with sway

--- config/nixos/gui.nix
+++ config/nixos/gui.nix
@@ -12,24 +12,7 @@
   # Permission escalation.
   security.polkit.enable = true;
 
-  services.autorandr.enable = true;
-
   # Enable secrets store.
   services.gnome3.gnome-keyring.enable = true;
   security.pam.services.lightdm.enableGnomeKeyring = true;
-
-  services.xserver = {
-    enable = true;
-    autorun = true;
-
-    # Start a DBus session.
-    startDbusSession = true;
-
-    displayManager.lightdm = {
-      enable = true;
-      greeters.gtk.enable = true;
-    };
-  };
-
-  services.gnome3.at-spi2-core.enable = true;
 }
--- config/nixos/gui/i3.nix
+++ config/nixos/gui/i3.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ... }:
 
 {
+  services.autorandr.enable = true;
+
   services.xserver = {
+    enable = true;
+    autorun = true;
+
+    # Start a DBus session.
+    startDbusSession = true;
+
     windowManager = {
       i3 = {
         enable = true;
@@ -12,6 +20,11 @@
 
     desktopManager = {
       xterm.enable = false;
+    };
+
+    displayManager.lightdm = {
+      enable = true;
+      greeters.gtk.enable = true;
     };
   };
 
