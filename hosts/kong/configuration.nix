65cfdb2de7a5dc850639b999ff0bdfe0d4c31fa2
Terje Larsen <terlar@gmail.com>
2020-02-19 07:53:58 +0100

    hosts/kong: fix kmonad input

--- hosts/kong/configuration.nix
+++ hosts/kong/configuration.nix
@@ -119,8 +119,8 @@ in {
     enable = true;
     xkbVariant = "altgr-intl";
     xkbOptions = "lv3:ralt_switch,ctrl:nocaps";
-    xkbRepeatDelay = 340;
-    xkbRepeatInterval = 170;
+    xkbRepeatDelay = 500;
+    xkbRepeatInterval = 33; # 30Hz
   };
 
   # Shell.
