436ed41966f61cbed5286fa81dc8e93e3df50dee
Terje Larsen <terlar@gmail.com>
2020-03-02 17:40:45 +0100

    overlays/python-packages: add custom packages to all python 3 versions

--- overlays/python-packages.nix
+++ overlays/python-packages.nix
@@ -1,11 +1,9 @@
 self: super:
 
-{
-  # https://github.com/NixOS/nixpkgs/issues/44426
-  python27 = super.python27.override { packageOverrides = self.pythonOverrides; };
-  python35 = super.python35.override { packageOverrides = self.pythonOverrides; };
-  python36 = super.python36.override { packageOverrides = self.pythonOverrides; };
-  python37 = super.python37.override { packageOverrides = self.pythonOverrides; };
+with builtins;
+with super.stdenv.lib;
 
-  pythonOverrides = import ../pkgs/python-packages;
-}
+let
+  packageOverrides = import ../pkgs/python-packages;
+  python3s = filterAttrs (n: v: match "python[3][0-9]" n != null) super;
+in mapAttrs (n: v: v.override { inherit packageOverrides; }) python3s
