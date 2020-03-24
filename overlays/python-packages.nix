54a6fd0c0b6047aabff8edc409f437bc12102609
Terje Larsen <terlar@gmail.com>
2020-03-24 21:50:29 +0100

    overlays/python-packages: only override python3 to add packages
    This was a notorious slow-down! Note to self, don't loop over and filter
    the whole root package set in overlays.

--- overlays/python-packages.nix
+++ overlays/python-packages.nix
@@ -5,5 +5,6 @@ with super.stdenv.lib;
 
 let
   packageOverrides = import ../pkgs/python-packages;
-  python3s = filterAttrs (n: v: match "python[3][0-9]" n != null) super;
-in mapAttrs (n: v: v.override { inherit packageOverrides; }) python3s
+in {
+  python3 = super.python3.override { inherit packageOverrides; };
+}
