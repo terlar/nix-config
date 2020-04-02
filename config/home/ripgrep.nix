f9d06fa6bf308d1e548610d191a44ee03b6d8bf4
Terje Larsen <terlar@gmail.com>
2020-04-02 17:40:00 +0200

    config/home/ripgrep: fix config loading

--- config/home/ripgrep.nix
+++ config/home/ripgrep.nix
@@ -1,7 +1,8 @@
-{ ... }:
+{ config, ... }:
 
 {
-  home.file.".ripgreprc".text = ''
+  home.sessionVariables.RIPGREP_CONFIG_PATH = "${config.xdg.configHome}/ripgrep/config";
+  xdg.configFile."ripgrep/config".text = ''
     --max-columns=150
     --max-columns-preview
 
