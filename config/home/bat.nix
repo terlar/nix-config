f488c0610dd8bf83737ae673f339c1f471ec7a3e
Terje Larsen <terlar@gmail.com>
2020-04-16 09:12:30 +0200

    config/home/bat: init

---
+++ config/home/bat.nix
@@ -0,0 +1,11 @@
+{ ... }:
+
+{
+  programs.bat = {
+    enable = true;
+    config = {
+      theme = "GitHub";
+      pager = "less -FR";
+    };
+  };
+}
--- config/home/development.nix
+++ config/home/development.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    ./bat.nix
     ./ripgrep.nix
   ];
 
