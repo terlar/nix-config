199a25077c27f4e60f49ab77519bdfbe962f16c7
Terje Larsen <terlar@gmail.com>
2020-04-23 21:11:36 +0200

    config/home/development: javascript config

--- config/home/development.nix
+++ config/home/development.nix
@@ -6,5 +6,15 @@
     ./ripgrep.nix
   ];
 
+  # JavaScript
+  home.file.".npmrc".text = ''
+    ignore-scripts=true
+  '';
+  home.file.".yarnrc".text = ''
+    disable-self-update-check true
+    ignore-scripts true
+  '';
+
+  # Python
   home.file.".ipython/profile_default/ipython_config.py".source = <dotfiles/python/.ipython/profile_default/ipython_config.py> ;
 }
