57ba2b5ab315d13ca7c0a58a2d5a0abca03f3ca6
Terje Larsen <terlar@gmail.com>
2020-05-21 16:55:00 +0200

    config/home/emacs/config: improve nix expression

--- config/home/emacs/config.nix
+++ config/home/emacs/config.nix
@@ -9,43 +9,40 @@
 let
   emacsPackages = (emacsPackagesGen emacs).overrideScope' overrides;
   emacsEnv = emacsPackages.emacsWithPackages extraPackages;
+
+  trivialBuild = emacsPackages.trivialBuild.override { emacs = emacsEnv; };
+
+  filteredSrc = let
+    filter = name: type: type != "symlink";
+  in builtins.filterSource filter src;
 in stdenv.mkDerivation rec {
   pname = "emacs-config";
   version = "1";
 
-  inherit src;
+  src = filteredSrc;
   dontUnpack = true;
 
-  buildInputs = [ emacsEnv ];
-
-  init = emacsPackages.trivialBuild {
+  init = trivialBuild {
     pname = "config-init";
-    inherit version src buildInputs;
+    inherit version src;
 
     preBuild = ''
-      find -type l -delete
-
-      mkdir -p .xdg-config
-      ln -s $PWD .xdg-config/emacs
-      export XDG_CONFIG_HOME="$PWD/.xdg-config"
-
-      export PATH="${emacsEnv}/bin:$PATH"
-
+      # Tangle org files
       emacs --batch -Q \
         *.org \
         -f org-babel-tangle
+
+      # Fake config directory in order to have files on load-path
+      mkdir -p .xdg-config
+      ln -s $PWD .xdg-config/emacs
+      export XDG_CONFIG_HOME="$PWD/.xdg-config"
     '';
   };
 
-  lisp = emacsPackages.trivialBuild {
+  lisp = trivialBuild {
     pname = "config-lisp";
     src = "${src}/lisp";
-
-    inherit version buildInputs;
-
-    preBuild = ''
-      export PATH="${emacsEnv}/bin:$PATH"
-    '';
+    inherit version;
   };
 
   installPhase = ''
