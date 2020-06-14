a613144ea678a4051a2a7663118e733542d8e184
Terje Larsen <terlar@gmail.com>
2020-06-14 21:33:05 +0200

    pkgs/rufo: 0.11.0 -> 0.12.0

--- pkgs/rufo/gemset.nix
+++ pkgs/rufo/gemset.nix
@@ -1,22 +1,12 @@
 {
-  rake = {
-    groups = [ "default" ];
-    platforms = [ ];
-    source = {
-      remotes = [ "https://rubygems.org" ];
-      sha256 = "0w6qza25bq1s825faaglkx1k6d59aiyjjk3yw3ip5sb463mhhai9";
-      type = "gem";
-    };
-    version = "13.0.1";
-  };
   rufo = {
-    groups = [ "default" ];
-    platforms = [ ];
+    groups = ["default"];
+    platforms = [];
     source = {
-      remotes = [ "https://rubygems.org" ];
-      sha256 = "0cyj3wji8qbzvxin2vg7g4jjfgps7jyfkzrxq37wnxfdsikys1mq";
+      remotes = ["https://rubygems.org"];
+      sha256 = "0nwasskcm0nrf7f52019x4fvxa5zckj4fcvf4cdl0qflrcwb1l9f";
       type = "gem";
     };
-    version = "0.11.0";
+    version = "0.12.0";
   };
 }
