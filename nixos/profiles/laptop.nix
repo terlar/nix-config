3d667af7e1853c58e42607d8dc76416142861af5
Terje Larsen <terlar@gmail.com>
2020-07-08 09:10:55 +0200

    Revert "nixos/profiles/laptop: no auto-switch on new device for pulseaudio"
    This reverts commit b57a128dba1919e6692453167d3a26604c15f650.

--- nixos/profiles/laptop.nix
+++ nixos/profiles/laptop.nix
@@ -12,6 +12,9 @@
       zeroconf.discovery.enable = true;
       extraModules = [ pkgs.pulseaudio-modules-bt ];
       package = pkgs.pulseaudioFull;
+      extraConfig = ''
+        load-module module-switch-on-connect
+      '';
     };
   };
 
