7c0833dbdedc0cb30cdac37dd0b8fd1076070df1
Terje Larsen <terlar@gmail.com>
2020-08-02 20:55:45 +0200

    overlays/gnome-extensions: use prev

--- overlays/gnome-extensions/default.nix
+++ overlays/gnome-extensions/default.nix
@@ -1,10 +1,7 @@
 final: prev:
 
-with prev;
-with gnomeExtensions;
-
 {
-  gnomeExtensions = gnomeExtensions // (with final.gnome3; {
+  gnomeExtensions = prev.gnomeExtensions // (with prev.gnome3; {
     gtktitlebar = callPackage ../../pkgs/gnome-extensions/gtktitlebar { };
     switcher = callPackage ../../pkgs/gnome-extensions/switcher { };
     invert-window = callPackage ../../pkgs/gnome-extensions/invert-window { };
