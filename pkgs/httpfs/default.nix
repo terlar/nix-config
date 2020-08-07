517562258b4f32bd6956e63b30763e9600442dda
Terje Larsen <terlar@gmail.com>
2020-08-07 16:51:35 +0200

    pkgs/httpfs: add TLS support

--- pkgs/httpfs/default.nix
+++ pkgs/httpfs/default.nix
@@ -1,5 +1,5 @@
-{ fetchurl, stdenv, pkgconfig, fuse, openssl, asciidoc
-, docbook_xml_dtd_45, docbook_xsl , libxml2, libxslt }:
+{ fetchurl, pkgconfig, stdenv, fuse, openssl, asciidoc, docbook_xml_dtd_45
+, docbook_xsl, gnutls, libxml2, libxslt }:
 
 stdenv.mkDerivation rec {
   name = "httpfs2-0.1.5";
@@ -9,18 +9,25 @@ stdenv.mkDerivation rec {
     sha256 = "1h8ggvhw30n2r6w11n1s458ypggdqx6ldwd61ma4yd7binrlpjq1";
   };
 
-  buildInputs =
-    [ pkgconfig fuse openssl
-      asciidoc docbook_xml_dtd_45 docbook_xsl libxml2 libxslt
-    ];
+  buildInputs = [
+    pkgconfig
+    fuse
+    openssl
+    asciidoc
+    docbook_xml_dtd_45
+    docbook_xsl
+    gnutls
+    libxml2
+    libxslt
+  ];
 
-  installPhase =
-    '' mkdir -p "$out/bin"
-       cp -v httpfs2 "$out/bin"
+  installPhase = ''
+    mkdir -p "$out/bin"
+    cp -v httpfs2{,-mt,-ssl} "$out/bin"
 
-       mkdir -p "$out/share/man/man1"
-       cp -v *.1 "$out/share/man/man1"
-    '';
+    mkdir -p "$out/share/man/man1"
+    cp -v *.1 "$out/share/man/man1"
+  '';
 
   meta = {
     description = "HTTPFS2, a FUSE-based HTTP file system for Linux";
