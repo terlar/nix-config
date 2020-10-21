daa094e340abbc25a3d186ff962f48e07083ca65
Terje Larsen <terlar@gmail.com>
2020-10-21 15:26:09 +0200

    nixos/caches: replace all-hies with ghcide-nix

---
+++ nixos/caches/ghcide-nix.nix
@@ -0,0 +1,8 @@
+{
+  nix = {
+    binaryCaches = [ "https://ghcide-nix.cachix.org" ];
+    binaryCachePublicKeys = [
+      "ghcide-nix.cachix.org-1:ibAY5FD+XWLzbLr8fxK6n8fL9zZe7jS+gYeyxyWYK5c="
+    ];
+  };
+}
