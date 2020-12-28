905528bacb0e0a8f46726cd2295350df2e75585e
Terje Larsen <terlar@gmail.com>
2020-12-28 15:27:41 +0100

    nixos/caches: remove project specific caches

--- nixos/caches/ghcide-nix.nix
+++
@@ -1,8 +0,0 @@
-{
-  nix = {
-    binaryCaches = [ "https://ghcide-nix.cachix.org" ];
-    binaryCachePublicKeys = [
-      "ghcide-nix.cachix.org-1:ibAY5FD+XWLzbLr8fxK6n8fL9zZe7jS+gYeyxyWYK5c="
-    ];
-  };
-}
