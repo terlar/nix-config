31e4e39cf9b69a3536cebe372688e0224f9dec87
Terje Larsen <terlar@gmail.com>
2021-02-02 10:42:08 +0100

    lib/scripts: update switch-home

--- lib/scripts.nix
+++ lib/scripts.nix
@@ -10,8 +10,22 @@ with pkgs;
 
   switchHome = writeShellScriptBin "switch-home" ''
     set -euo pipefail
-    home-manager -b bak switch $@
-    echo "Home generation: $(home-manager generations | head -1)"
+    export PATH=${lib.makeBinPath [ gitMinimal jq nixUnstable ]}
+    usr="''${1:-$USER}"
+
+    1>&2 echo "Switching Home Manager configuration for: $usr"
+
+    usrExists="$(nix eval --json .#homeManagerConfigurations --apply 'x: (builtins.any (n: n == "'$usr'") (builtins.attrNames x))' 2>/dev/null)"
+
+    if [ "$usrExists" != "true" ]; then
+      1>&2 echo "No configuration found, aborting..."
+      exit 1
+    fi
+
+    1>&2 echo "Building configuration..."
+    out="$(nix build --json ".#homeManagerConfigurations.$usr.activationPackage" | jq -r .[].outputs.out)"
+    1>&2 echo "Activating configuration..."
+    "$out"/activate
   '';
 
   installQutebrowserDicts = writeShellScriptBin "install-qb-dicts" ''
