1caf28bf9259da023773ad9f0a3fc966b3624a9c
Terje Larsen <terlar@gmail.com>
2021-02-03 11:59:26 +0100

    flake: move nixGL wrapping into overlay

--- flake.nix
+++ flake.nix
@@ -88,13 +88,51 @@
 
         pkgsForSystem = { system, extraOverlays ? [ ] }:
           let
-            nixOverlay = self: super: {
+            nixOverlay = final: prev: {
               nixUnstable = nix.defaultPackage.${system};
             };
 
-            nixGLOverlay = self: super:
-              let nixGL = import inputs.nixGL { pkgs = self; };
-              in { inherit (nixGL) nixGLNvidia nixGLIntel nixGLDefault; };
+            nixGLOverlay = final: prev:
+              let
+                nixGL = import inputs.nixGL { pkgs = final; };
+                wrapWithNixGL = wrapper: package:
+                  let
+                    getBinFiles = pkg:
+                      lib.pipe "${lib.getBin pkg}/bin" [
+                        readDir
+                        attrNames
+                        (filter (n: match "^\\..*" n == null))
+                      ];
+
+                    wrapperBin = lib.pipe wrapper [
+                      getBinFiles
+                      (filter (n: n == (lib.getName wrapper)))
+                      head
+                      (x: "${wrapper}/bin/${x}")
+                    ];
+
+                    binFiles = getBinFiles package;
+                    wrapBin = name:
+                      final.writeShellScriptBin name ''
+                        exec ${wrapperBin} ${package}/bin/${name} "$@"
+                      '';
+                  in final.symlinkJoin {
+                    name = "${package.name}-nixgl";
+                    paths = (map wrapBin binFiles) ++ [ package ];
+                  };
+
+                wrappers = let
+                  replacePrefix =
+                    replaceStrings [ "wrapWithNixGL" ] [ "nixGL" ];
+                in lib.genAttrs [
+                  "wrapWithNixGLNvidia"
+                  "wrapWithNixGLIntel"
+                  "wrapWithNixGLDefault"
+                ] (name: wrapWithNixGL final.${replacePrefix name});
+              in {
+                inherit (nixGL) nixGLNvidia nixGLIntel nixGLDefault;
+                inherit wrapWithNixGL;
+              } // wrappers;
 
             inputOverlays = [
               nixOverlay
--- home-manager/modules/targets/generic-linux.nix
+++ home-manager/modules/targets/generic-linux.nix
@@ -6,103 +6,24 @@ with lib;
 let
   cfg = config.targets.genericLinux;
   profileDirectory = config.home.profileDirectory;
-
-  openglOpts = {
-    options = {
-      wrapperPackage = mkOption {
-        type = types.nullOr types.package;
-        default = null;
-        example = literalExample "pkgs.nixGLIntel";
-        description = "Package used for OpenGL wrapping.";
-      };
-
-      wrapPackages = mkOption {
-        type = types.listOf types.package;
-        default = [ ];
-        example = literalExample ''
-          with pkgs; [
-            alacritty
-            kitty
-          ]
-        '';
-
-        description = ''
-          List of packages that needs to be wrapped with a OpenGL wrapper.
-        '';
-      };
-    };
-  };
 in {
-  options.targets.genericLinux = {
-    opengl = mkOption {
-      type = types.nullOr (types.submodule openglOpts);
-      default = null;
-      example = literalExample ''
-        {
-          wrapperPackage = pkgs.nixGLIntel;
-          wrapPackages = [ pkgs.kitty ];
-        }
-      '';
-      description = ''
-        OpenGL compatibility configuration.
-      '';
-    };
-  };
+  config = mkIf cfg.enable (mkMerge [{
+    systemd.user.sessionVariables = let
+      profiles =
+        [ "\${NIX_STATE_DIR:-/nix/var/nix}/profiles/default" profileDirectory ];
+      dataDirs = concatStringsSep ":"
+        (map (profile: "${profile}/share") profiles ++ cfg.extraXdgDataDirs);
+    in { XDG_DATA_DIRS = "${dataDirs}\${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"; };
 
-  config = mkIf cfg.enable (mkMerge [
-    {
-      systemd.user.sessionVariables = let
-        profiles = [
-          "\${NIX_STATE_DIR:-/nix/var/nix}/profiles/default"
-          profileDirectory
-        ];
-        dataDirs = concatStringsSep ":"
-          (map (profile: "${profile}/share") profiles ++ cfg.extraXdgDataDirs);
-      in { XDG_DATA_DIRS = "${dataDirs}\${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"; };
-
-      programs.fish.shellInit = ''
-        set --prepend fish_function_path ${
-          if pkgs ? fishPlugins && pkgs.fishPlugins ? foreign-env then
-            "${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d"
-          else
-            "${pkgs.fish-foreign-env}/share/fish-foreign-env/functions"
-        }
-        fenv source ${pkgs.nix}/etc/profile.d/nix.sh > /dev/null
-        set -e fish_function_path[1]
-      '';
-    }
-    (mkIf (cfg.opengl != null && cfg.opengl.wrapperPackage != null
-      && cfg.opengl.wrapPackages != [ ]) {
-        home.packages = [ cfg.opengl.wrapperPackage ];
-
-        nixpkgs.config.packageOverrides = let
-          packageNames = map getName cfg.opengl.wrapPackages;
-
-          getBinFiles = pkg:
-            pipe "${getBin pkg}/bin" [
-              readDir
-              attrNames
-              (filter (n: match "^\\..*" n == null))
-            ];
-
-          wrapperBin = pipe cfg.opengl.wrapperPackage [
-            getBinFiles
-            (filter (n: n == (getName cfg.opengl.wrapperPackage)))
-            head
-          ];
-
-          wrapBinFiles = pkg:
-            let
-              binFiles = getBinFiles pkg;
-              wrapBin = name:
-                pkgs.writeShellScriptBin name ''
-                  exec ${wrapperBin} ${pkg}/bin/${name} "$@"
-                '';
-            in pkgs.symlinkJoin {
-              name = "${pkg.name}-nixgl";
-              paths = (map wrapBin binFiles) ++ [ pkg ];
-            };
-        in genAttrs packageNames (name: wrapBinFiles pkgs.${name});
-      })
-  ]);
+    programs.fish.shellInit = ''
+      set --prepend fish_function_path ${
+        if pkgs ? fishPlugins && pkgs.fishPlugins ? foreign-env then
+          "${pkgs.fishPlugins.foreign-env}/share/fish/vendor_functions.d"
+        else
+          "${pkgs.fish-foreign-env}/share/fish-foreign-env/functions"
+      }
+      fenv source ${pkgs.nix}/etc/profile.d/nix.sh > /dev/null
+      set -e fish_function_path[1]
+    '';
+  }]);
 }
