3515bf4bb12b8daf103620f60b817c0685339351
Terje Larsen <terlar@gmail.com>
2021-02-17 22:25:49 +0100

    overlays/iosevka: update config format

--- overlays/iosevka.nix
+++ overlays/iosevka.nix
@@ -2,23 +2,21 @@ final: prev:
 
 {
   iosevka-slab = prev.iosevka.override {
+    set = "slab";
     privateBuildPlan = {
       family = "Iosevka Slab";
-      design = [
-        "slab"
-        "ligset-hskl"
-        "calt-exeq-alt-1"
-        "calt-html-comment"
-        "calt-tildeeq"
-        "v-asterisk-low"
-        "v-at-long"
-        "v-caret-low"
-        "v-dollar-open"
-        "v-paragraph-low"
-        "v-underscore-low"
-        "v-zero-dotted"
-      ];
+      spacing = "normal";
+      serifs = "slab";
+
+      variants.design = {
+        asterisk = "low";
+        at = "threefold";
+        caret = "low";
+        dollar = "open";
+        paragraph-sign = "low";
+        underscore = "low";
+        zero = "dotted";
+      };
     };
-    set = "slab";
   };
 }
