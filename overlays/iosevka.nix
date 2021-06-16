f2361e60f189d272d9faad2572df7e924556e3ec
Terje Larsen <terlar@gmail.com>
2021-06-16 22:14:17 +0200

    overlays/iosevka: fix config

--- overlays/iosevka.nix
+++ overlays/iosevka.nix
@@ -5,11 +5,11 @@ final: prev:
     set = "slab";
     privateBuildPlan = {
       family = "Iosevka Slab";
-      spacing = "normal";
+      spacing = "fontconfig-mono";
       serifs = "slab";
 
       variants.design = {
-        asterisk = "low";
+        asterisk = "hex-low";
         at = "threefold";
         caret = "low";
         dollar = "open";
