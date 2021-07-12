e152e38f803b83b0afe8d2b7f9d9c5e5bd2767f4
Terje Larsen <terlar@gmail.com>
2021-07-12 12:56:50 +0200

    flake (nixosConfigurations.snail): init

--- flake.nix
+++ flake.nix
@@ -243,6 +243,59 @@
             ];
             specialArgs = { inherit (inputs) dotfiles; };
           };
+
+          snail = {
+            system = "aarch64-linux";
+
+            configuration = { pkgs, ... }: {
+              hardware.enableRedistributableFirmware = true;
+
+              networking = {
+                hostName = "snail";
+                wireless.enable = true;
+              };
+
+              boot = {
+                kernelParams = [
+                  "console=ttyS1,115200n8"
+                ];
+
+                loader.raspberryPi = {
+                  enable = true;
+                  version = 3;
+                  uboot.enable = true;
+                  firmwareConfig = ''
+                    start_x=1
+                    gpu_mem=256
+                  '';
+                };
+              };
+
+              services.openssh.enable = true;
+
+              services.unifi = {
+                enable = true;
+                unifiPackage = pkgs.unifi;
+              };
+
+              users.users.root.openssh.authorizedKeys.keys = [
+                "your public key here"
+              ];
+
+              systemd.services.btattach = {
+                before = [ "bluetooth.service" ];
+                after = [ "dev-ttyAMA0.device" ];
+                wantedBy = [ "multi-user.target" ];
+                serviceConfig = {
+                  ExecStart = "${pkgs.bluez}/bin/btattach -B /dev/ttyAMA0 -P bcm -S 3000000";
+                };
+              };
+
+              environment.systemPackages = [
+                pkgs.emacs
+              ];
+            };
+          };
         };
       };
 
@@ -257,6 +310,7 @@
       nixosConfigurations = {
         beetle = self.lib.nixosSystem self.lib.nixosHosts.beetle;
         kong = self.lib.nixosSystem self.lib.nixosHosts.kong;
+        snail = self.lib.nixosSystem self.lib.nixosHosts.snail;
         yubikey-installer = self.lib.nixosInstaller {
           configuration = ./nixos/installer/yubikey;
         };
