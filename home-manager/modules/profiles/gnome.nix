21bdd7eed1c294e7868985cd4af78b07e1323230
Terje Larsen <terlar@gmail.com>
2021-08-02 10:07:25 +0200

    home-manager/modules/profiles/gnome: add install-gnome-session script

--- home-manager/modules/profiles/gnome.nix
+++ home-manager/modules/profiles/gnome.nix
@@ -1,7 +1,12 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-let cfg = config.profiles.gnome;
+let
+  cfg = config.profiles.gnome;
+  installGnomeSessionScript = pkgs.writeShellScriptBin "install-gnome-session" ''
+    sudo ln -fs ${pkgs.gnome.gnome-session.sessions}/share/wayland-sessions/gnome.desktop /usr/share/wayland-sessions/gnome.desktop
+    sudo ln -fs ${pkgs.gnome.gnome-session.sessions}/share/xsessions/gnome.desktop /usr/share/xsessions/gnome.desktop
+  '';
 in
 {
   options.profiles.gnome = {
@@ -20,6 +25,8 @@ in
       dconf.enable = true;
 
       services.gpg-agent.pinentryFlavor = "gnome3";
+
+      home.packages = [ installGnomeSessionScript ];
     }
     (mkIf cfg.disableKeyringSshAgent {
       xdg.configFile."autostart/gnome-keyring-ssh.desktop".text = ''
