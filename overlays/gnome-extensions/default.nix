74f7ac12565a0afa8e421ba27bfc9209c8b66588
Terje Larsen <terlar@gmail.com>
2021-11-08 22:33:31 +0100

    overlays/gnome-extensions: update material-shell

--- overlays/gnome-extensions/default.nix
+++ overlays/gnome-extensions/default.nix
@@ -2,18 +2,30 @@ final: prev:
 
 {
   gnomeExtensions = prev.gnomeExtensions // (with prev.gnome3; {
-    paperwm = prev.gnomeExtensions.paperwm.overrideDerivation
-      (old:
-        let version = "pre-40.0"; in
-        {
-          inherit version;
-          name = "${old.pname}-${version}";
-          src = prev.fetchFromGitHub {
-            owner = "paperwm";
-            repo = "paperwm";
-            rev = "e9f714846b9eac8bdd5b33c3d33f1a9d2fbdecd4";
-            sha256 = "sha256-gZbS2Xy+CuQfzzZ5IwMahr3VLtyTiLxJTJVawml9sXE=";
-          };
-        });
+    material-shell = prev.gnomeExtensions.material-shell.overrideDerivation (old:
+      let version = "40.a"; in
+      {
+        inherit version;
+        name = "${old.pname}-${version}";
+        src = prev.fetchFromGitHub {
+          owner = "material-shell";
+          repo = "material-shell";
+          rev = "f44335d07f72f8b3fe984c3390c70d51cca30018";
+          sha256 = "sha256-Plzjid2X6C5NM/OWaMk78FKUwNT1taubFBtYw1dNIRw=";
+        };
+      });
+
+    paperwm = prev.gnomeExtensions.paperwm.overrideDerivation (old:
+      let version = "pre-40.0"; in
+      {
+        inherit version;
+        name = "${old.pname}-${version}";
+        src = prev.fetchFromGitHub {
+          owner = "paperwm";
+          repo = "paperwm";
+          rev = "e9f714846b9eac8bdd5b33c3d33f1a9d2fbdecd4";
+          sha256 = "sha256-gZbS2Xy+CuQfzzZ5IwMahr3VLtyTiLxJTJVawml9sXE=";
+        };
+      });
   });
 }
