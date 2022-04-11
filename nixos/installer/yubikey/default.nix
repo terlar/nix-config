b6100b75ea57a21de158ab83acadbbbeedc9db8d
Terje Larsen <terlar@gmail.com>
2022-04-11 03:47:34 +0200

    nixos/installer/yubikey: update and add workflow to test

--- nixos/installer/yubikey/default.nix
+++ nixos/installer/yubikey/default.nix
@@ -1,13 +1,20 @@
 { pkgs, ... }:
 
+let
+  drduh-gpg-conf = pkgs.callPackage ./drduh-gpg-conf.nix { };
+  gpg-agent-conf = pkgs.writeText "gpg-agent.conf" ''
+    pinentry-program ${pkgs.pinentry-curses}/bin/pinentry-curses
+  '';
+in
 {
+  # Required packages and services.
   services.pcscd.enable = true;
   services.udev.packages = [ pkgs.yubikey-personalization ];
-  environment.systemPackages = with pkgs; [
-    gnupg
-    pinentry-curses
-    paperkey
-    wget
+  environment.systemPackages = [
+    pkgs.gnupg
+    pkgs.pinentry-curses
+    pkgs.paperkey
+    pkgs.wget
   ];
 
   programs = {
@@ -17,4 +24,32 @@
       enableSSHSupport = true;
     };
   };
+
+  # Avoid accidental persistence to USB-drive
+  boot.kernelParams = [ "copytoram" ];
+
+  # Disable networking
+  boot.initrd.network.enable = false;
+  networking.dhcpcd.enable = false;
+  networking.dhcpcd.allowInterfaces = [ ];
+  networking.firewall.enable = true;
+  networking.useDHCP = false;
+  networking.useNetworkd = false;
+  networking.wireless.enable = false;
+
+  # Secure defaults.
+  boot.cleanTmpDir = true;
+  boot.kernel.sysctl = {
+    "kernel.unprivileged_bpf_disabled" = 1;
+  };
+
+  # Set up the shell for making keys.
+  environment.interactiveShellInit = ''
+    unset HISTFILE
+    export GNUPGHOME=/run/user/$(id -u)/gnupg
+    [ -d $GNUPGHOME ] || install -m 0700 -d $GNUPGHOME
+    cp ${drduh-gpg-conf}/gpg.conf $GNUPGHOME/gpg.conf
+    cp ${gpg-agent-conf}  $GNUPGHOME/gpg-agent.conf
+    echo "\$GNUPGHOME is $GNUPGHOME"
+  '';
 }
---
+++ nixos/installer/yubikey/drduh-gpg-conf.nix
@@ -0,0 +1,26 @@
+{ stdenv, lib, fetchFromGitHub }:
+
+stdenv.mkDerivation {
+  pname = "drduh-gpg-conf";
+  version = "20210215";
+
+  src = fetchFromGitHub {
+    owner = "drduh";
+    repo = "config";
+    rev = "2703f5992be264e993a46802169a76e7211d9ad0";
+    hash = "sha256-ui3VylKFd68sV/PligG+7S86Xi+TGYOoeekBDFvBwJY=";
+  };
+
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir $out
+    cp $src/gpg.conf $out/gpg.conf
+  '';
+
+  meta = with lib; {
+    description = "drduh's gpg.conf";
+    homepage = https://github.com/drduh/config;
+    license = licenses.mit;
+  };
+}
