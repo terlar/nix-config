dc472649981fe4c9b1ad20edc8ad677968a65a52
Terje Larsen <terlar@gmail.com>
2022-09-05 11:47:55 +0200

    overlays/gnupg: use stable patch URL

--- overlays/gnupg.nix
+++ overlays/gnupg.nix
@@ -7,8 +7,8 @@ final: prev:
         (final.fetchpatch {
           name = "fix-yubikey.patch";
           url =
-            "https://files.gnupg.net/file/data/io6l7fvfebflbqm44ll3/PHID-FILE-7ppyegfowo5do2d6izc4/file";
-          sha256 = "sha256-J/PLSz8yiEgtGv+r3BTGTHrikV70AbbHQPo9xbjaHFE=";
+              "https://git.gnupg.org/cgi-bin/gitweb.cgi?p=gnupg.git;a=patch;h=f34b9147eb3070bce80d53febaa564164cd6c977;hp=95651d1a4fec9bc5e36f623b2cdcc6a35e0c30bb";
+          sha256 = "sha256-KD8H6N9oGx+85SetHx1OJku28J2dmsj/JkJudknCueU=";
         })
       ];
     } else
