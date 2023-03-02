f67ccfcd6cf8bc56046d5a91f4feb731c03e4a8b
Terje Larsen <terlar@gmail.com>
2023-03-02 05:16:25 +0100

    hm/profiles/u/terje/gnome: configure gnome-keyring

--- home-manager/modules/profiles/user/terje/gnome.nix
+++ home-manager/modules/profiles/user/terje/gnome.nix
@@ -28,6 +28,11 @@ in {
     {
       profiles.gnome.enable = true;
 
+      services.gnome-keyring = {
+        enable = true;
+        components = ["pkcs11" "secrets"];
+      };
+
       programs.gnome-shell = {
         enable = true;
         extensions = [
