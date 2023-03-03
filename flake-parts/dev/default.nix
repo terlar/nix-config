c095a04c1e2d45f319561910099e5e64597a0a67
Terje Larsen <terlar@gmail.com>
2023-03-03 01:09:19 +0100

    flake-parts: use devshell and treefmt-nix integration

--- flake-parts/dev/default.nix
+++ flake-parts/dev/default.nix
@@ -1,18 +1,58 @@
 {inputs, ...}: {
-  imports = [inputs.pre-commit-hooks.flakeModule];
+  imports = [
+    inputs.devshell.flakeModule
+    inputs.pre-commit-hooks.flakeModule
+    inputs.treefmt.flakeModule
+  ];
 
-  perSystem = {pkgs, ...}: {
-    imports = [./devshell.nix];
-
+  perSystem = {
+    config,
+    pkgs,
+    ...
+  }: {
     formatter = pkgs.alejandra;
+    treefmt.programs.alejandra.enable = true;
+
+    treefmt = {
+      flakeFormatter = false;
+      projectRootFile = "flake.nix";
+    };
 
     pre-commit = {
       check.enable = true;
       settings.hooks = {
-        alejandra.enable = true;
         deadnix.enable = true;
         statix.enable = true;
+        treefmt.enable = true;
       };
+    };
+
+    devshell.shells.default = {
+      name = "terlar/nix-config";
+      devshell.startup.pre-commit-install.text = config.pre-commit.installationScript;
+
+      packages = [
+        pkgs.git
+        pkgs.nixVersions.stable
+      ];
+
+      commands = [
+        {
+          name = "repl";
+          command = ''
+            exec nix repl repl.nix "$@"
+          '';
+          category = "development";
+          help = "Development REPL";
+        }
+      ];
+
+      env = [
+        {
+          name = "NIX_USER_CONF_FILES";
+          value = toString ../../nix.conf;
+        }
+      ];
     };
   };
 }
