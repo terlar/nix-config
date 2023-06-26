60365a8ccc26f26e526bbb93b79d812d6e1a032f
Terje Larsen <terlar@gmail.com>
2023-06-26 04:39:43 +0200

    nixos/profiles/hardware/yubikey: remove WSL hack

--- nixos/profiles/hardware/yubikey.nix
+++ nixos/profiles/hardware/yubikey.nix
@@ -5,11 +5,7 @@
     pcscd.enable = true;
     udev = {
       enable = true;
-
-      # WSL+usbip compatibility fix.
-      extraRules = ''
-        SUBSYSTEM=="usb", ATTRS{idVendor}=="1050", ATTRS{idProduct}=="0010|0110|0111|0114|0116|0401|0403|0405|0407|0410", MODE="0666"
-      '';
+      packages = [pkgs.yubikey-personalization];
     };
   };
 }
