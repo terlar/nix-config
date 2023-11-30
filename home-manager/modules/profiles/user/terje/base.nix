dc8eadb3cf0f6e97335a505563af8daf95924185
Terje Larsen <terlar@gmail.com>
2023-11-30 15:33:40 +0000

    feat(home-manager): add glow config to user profile terje

--- home-manager/modules/profiles/user/terje/base.nix
+++ home-manager/modules/profiles/user/terje/base.nix
@@ -7,6 +7,9 @@
 }: let
   cfg = config.profiles.user.terje.base;
 
+  jsonFormat = pkgs.formats.json {};
+  yamlFormat = pkgs.formats.yaml {};
+
   sourceDirFiles = attrRoot: destination: target: {
     ${attrRoot} = builtins.foldl' (attrs: file:
       attrs
@@ -230,6 +233,98 @@ in {
       programs.fish.interactiveShellInit = ''
         nix-your-shell fish | source
       '';
+    }
+
+    # Glow
+    {
+      xdg.configFile."glow/glow.yml".source = let
+        styleFile = jsonFormat.generate "custom.json" {
+          document = {
+            block_prefix = "\n";
+            block_suffix = "\n";
+            margin = 2;
+          };
+
+          block_quote = {
+            indent = 1;
+            indent_token = "┃ ";
+          };
+          list = {
+            color = "0";
+            level_indent = 2;
+          };
+          heading = {
+            block_suffix = "\n";
+            bold = true;
+          };
+          h1.prefix = "# ";
+          h2.prefix = "## ";
+          h3.prefix = "### ";
+          h4.prefix = "#### ";
+          h5.prefix = "##### ";
+          h6 = {
+            prefix = "###### ";
+            bold = false;
+          };
+          strikethrough.crossed_out = true;
+          emph.italic = true;
+          strong.bold = true;
+          hr.format = "\n────────\n";
+          item.block_prefix = "• ";
+          enumeration.block_prefix = ". ";
+          task = {
+            ticked = "☑ ";
+            unticked = "□ ";
+          };
+          link = {
+            color = "#007ec4";
+            underline = true;
+          };
+          link_text = {
+            color = "#007ec4";
+            bold = true;
+          };
+          image = {
+            color = "#007ec4";
+            underline = true;
+          };
+          image_text = {
+            format = "Image: {{.text}} ?";
+          };
+          code = {
+            prefix = " ";
+            suffix = " ";
+            background_color = "11";
+          };
+          code_block = {
+            margin = 2;
+            chroma = {
+              text.background_color = "#f4eedb";
+              keyword.bold = true;
+              name_builtin.bold = true;
+              name_class = {
+                underline = true;
+                bold = true;
+              };
+              generic_emph.italic = true;
+              generic_strong.bold = true;
+            };
+          };
+          table = {
+            color = "0";
+            center_separator = "╂";
+            column_separator = "┃";
+            row_separator = "─";
+          };
+          definition_description = {
+            block_prefix = "\n► ";
+          };
+        };
+      in
+        yamlFormat.generate "glow.yml" {
+          style = styleFile;
+          pager = true;
+        };
     }
 
     # Packages
