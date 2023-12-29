93f37b084fdf6a18a3f5ee488768c649c29a0c80
Terje Larsen <terlar@gmail.com>
2023-12-29 09:01:45 +0100

    refactor: gnome config

--- packages/default.nix
+++ packages/default.nix
@@ -3,4 +3,12 @@ _final: prev: {
   project-init = prev.callPackage ./project-init {};
   saw = prev.callPackage ./saw {};
   themepark = prev.callPackage ./themepark {};
+
+  gnome =
+    prev.gnome
+    // {
+      gnome-keyring = prev.gnome.gnome-keyring.overrideAttrs (old: {
+        configureFlags = old.configureFlags ++ ["--disable-ssh-agent"];
+      });
+    };
 }
