38d9558a0e646bed92df02679a6ccd996342989c
Terje Larsen <terlar@gmail.com>
2024-02-14 10:27:20 +0100

    feat(package): remove thermald override

--- packages/default.nix
+++ packages/default.nix
@@ -1,14 +1,3 @@
 _final: prev: {
   iosevka-slab = prev.callPackage ./iosevka-slab {};
-
-  thermald = prev.thermald.overrideAttrs (old: {
-    patches =
-      (old.patches or [])
-      ++ [
-        (prev.fetchpatch {
-          url = "https://patch-diff.githubusercontent.com/raw/intel/thermal_daemon/pull/422.patch";
-          hash = "sha256-SDZBGVFAU8PYWfIkqLqprXrFfkympf9wg6elEugFk/g=";
-        })
-      ];
-  });
 }
