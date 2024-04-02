864d7c474c9d1e3f427c29fbc684b4a2c5fc13c6
Terje Larsen <terlar@gmail.com>
2024-04-02 14:43:03 +0200

    fix: update related issues

--- dev/flake.nix
+++ dev/flake.nix
@@ -8,7 +8,10 @@
     };
     dev-flake = {
       url = "github:terlar/dev-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        call-flake.url = "github:divnix/call-flake/a9bc85f5bd939734655327a824b4e7ceb4ccaba9";
+      };
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
