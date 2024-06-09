e27bcd7dddf810e37c0bd6565827f730715b3998
Terje Larsen <terlar@gmail.com>
2024-06-09 21:23:44 +0200

    chore(package): update paperwm

--- packages/default.nix
+++ packages/default.nix
@@ -1,8 +1,15 @@
-_: prev: {
-  gnome45Extensions = prev.gnome45Extensions // {
-    "paperwm@paperwm.github.com" = prev.gnome45Extensions."paperwm@paperwm.github.com".override (_: {
-      version = "99";
-      sha256 = "sha256-LyvCfDKKdLh/gL+pNyDzlYfNJUq5GYkGTDwZ0JLFU5c=";
-    });
+_: prev:
+let
+  paperwm = prev.gnomeExtensions.paperwm.override (_: {
+    version = "113";
+    sha256 = "sha256-RuLj4JLHPOKA/4g0Nmvr2Ti0oVw+qCUCJCxjNVe6cq0=";
+  });
+in
+{
+  gnomeExtensions = prev.gnomeExtensions // {
+    inherit paperwm;
+  };
+  gnome46Extensions = prev.gnome46Extensions // {
+    "paperwm@paperwm.github.com" = paperwm;
   };
 }
