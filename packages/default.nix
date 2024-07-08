7838e54c170594c2ca00d30cdd5226d37a2bc590
Terje Larsen <terlar@gmail.com>
2024-07-08 12:38:30 +0200

    chore: remove paperwm overlay

--- packages/default.nix
+++ packages/default.nix
@@ -1,15 +1,4 @@
-_: prev:
-let
-  paperwm = prev.gnomeExtensions.paperwm.override (_: {
-    version = "113";
-    sha256 = "sha256-RuLj4JLHPOKA/4g0Nmvr2Ti0oVw+qCUCJCxjNVe6cq0=";
-  });
-in
+_: _:
+
 {
-  gnomeExtensions = prev.gnomeExtensions // {
-    inherit paperwm;
-  };
-  gnome46Extensions = prev.gnome46Extensions // {
-    "paperwm@paperwm.github.com" = paperwm;
-  };
 }
