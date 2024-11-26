5d71c8339b7cb37f4b35b8618e5e927a02df45ec
Terje Larsen <terlar@gmail.com>
2024-11-26 13:06:52 +0100

    feat(nixos): configure ollama flash attention for kong

--- configurations/nixos/kong/configuration.nix
+++ configurations/nixos/kong/configuration.nix
@@ -101,6 +101,9 @@ in
     services.ollama = {
       enable = true;
       package = pkgs.ollama-cuda;
+      environmentVariables = {
+        OLLAMA_FLASH_ATTENTION = "1";
+      };
     };
   };
 
